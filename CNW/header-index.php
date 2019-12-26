26c9e35c9f1c3e4ab0625f31c5cfaf4b5f080d75
175A071345 <lyntt72@wru.vn>
2019-12-26 17:04:00 +0700

    Up btl

---
+++ CNW/header-index.php
@@ -0,0 +1,44 @@
+<header>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="head-top">
+				<div class="head-top-left col-md-9"><h1>HỆ THÔNG ĐĂNG KÝ HỌC - ĐẠI HỌC THỦY LỢI</h1></div>
+				<div class="head-top-right col-md-3">
+				<div style='color:blue; padding:5px 0px;'> Khách </div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+			
+			<div class="main-top">
+				<div class="left-top">
+				</div>
+				<div class="right-top">
+					<div id="menu">
+						<ul>
+							
+							<?php if(isset($_SESSION['Username'])){
+							echo '<li><a href="index.php" style="border-left: none">Trang chủ</a></li>';
+							echo '<li><a href="hoidap.php">Hỏi đáp</a></li>';
+							echo '<li><a href="">Trợ giúp</a></li>';
+							echo '<li><a href="logout.php">Thoát</a></li>';
+							} 
+							else{
+							echo '<li><a href="index.php" style="border-left: none">Trang chủ</a></li>';
+							echo '<li><a href="dangnhap.php">Đăng nhập</a></li>';
+							echo '<li><a href="hoidap.php">Hỏi đáp</a></li>';
+							echo '<li><a href="">Trợ giúp</a></li>';
+							}?>
+							
+							<li style="line-height: 30px;">
+								<select name="">
+									<option value="">VN</option>
+								</select>
+							</li>
+						</ul>
+					</div>
+					
+				</div>
+			</div>
+		</header>
---
+++ CNW/hoidap.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Đăng nhập</title>
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+      <!-- phần đầu -->
+  <?php
+    include"header-index.php";
+  ?>
+  <main>
+    <div class="hoi">
+    <h1>Hỏi đáp</h1>
+      <br>
+      <p>Phòng Đào tạo ĐH&SĐH (P3), Phòng Khảo thí và Kiểm định chất lượng (P4), Phòng Tài vụ (P5), Phòng Công tác chính trị và quản lý sinh viên (P7), Trung tâm tin học (TTTH) hỗ trợ, giải đáp những thông tin gì cho sinh viên?</p>
+      <br>
+      <h1>Đáp</h1><br>
+      <p>1. Phòng Đào tạo ĐH&SĐH (P3) hỗ trợ giải đáp cho sinh viên thông tin về: <br>
+- Chương trình đào tạo <br>
+- Kế hoạch đào tạo <br>
+- Môn học<br>
+- Thời khóa biểu<br>
+- Lịch thi<br>
+- Kế hoạch đăng ký học, đăng ký nguyện vọng<br>
+2. Phòng Khảo thí và Kiểm định chất lượng (P4) hỗ trợ giải đáp cho sinh viên thông tin về:<br>
+- Tổ chức thi<br>
+- Hoãn thi<br>
+- Phúc khảo bài thi<br>
+- Điểm thi<br>
+3. Phòng Tài vụ (P5) hỗ trợ giải đáp cho sinh viên thông tin về:<br>
+- Học phí<br>
+- Học bổng<br>
+- Miễn giảm học phí<br>
+4. Phòng Công tác chính trị và Quản lý sinh viên (P7) hỗ trợ giải đáp cho sinh viên thông tin về:<br>
+- Xử lý học vụ<br>
+- Khen thưởng, kỷ luật<br>
+- Xét học bổng<br>
+- Miễn giảm học phí<br>
+- Chế độ, chính sách<br>
+- Quy định, quy chế<br>
+- Xét giao thực tập tốt nghiệp (cho khối kinh tế)<br>
+- Xét giao học phần tốt nghiệp<br>
+- Xét tốt nghiệp<br>
+- Xác nhận các giấy tờ hồ sơ liên quan đến sinh viên.<br>
+- Hồ sơ sinh viên<br>
+- Tài khoản đăng ký học sinh viên (trạng thái tài khoản: Khóa tài khoản, bảo lưu, thôi học...). Lưu ý: Sinh viên quên mật khẩu tài khoản đăng ký học thì liên hệ Cố vấn học tập (kiêm giáo viên chủ nhiệm), trong trường hợp khóa sinh viên đã ra trường thì liên hệ Phòng Đào tạo ĐH&SĐH nhé.<br>
+5. Trung tâm tin học (TTTH) hỗ trợ giải đáp cho sinh viên thông tin về:<br>
+- Email (quên mật khẩu)</p>
+    </div>
+  </main>
+    <!-- phần cuối -->
+    <?php
+    include"footer-index.php";
+  ?>
