905cdf64fb449c9cc50ae3be6ad77310b2bed27e
175A071345 <lyntt72@wru.vn>
2019-12-30 22:06:35 +0700

    linh up

---
+++ BTL_web/formlogin.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/login.css">
+    <title>Trường Đại học Thủy Lợi</title>
+</head>
+<body>
+    <div class="header">
+        <h3>HỆ THỐNG ĐĂNG KÍ HỌC TRƯỜNG ĐẠI HỌC THỦY LỢI</h3>
+    </div>
+    <ul class="navigation">
+        <li><a href="index.php">Trang chủ</a></li>
+        <li><a href="hoidap.php">Hỏi đáp</a></li>
+        <li><a href="">Trợ giúp</a></li>
+    </ul>
+    <div class="main">
+        <form>
+            <h4>ĐĂNG NHẬP</h4>
+            <span>Tên tài khoản:</span><input type="text"><br><br>
+            <span>Mật khẩu:</span><input type="password"><br><br>
+            <div class="md3">
+				<div class="right-w3l">
+					<input type="submit" name="login" id="login" value="Đăng nhập">
+				</div> <br>
+				<div class="register"><b><a href="register.php"> Đăng ký</a></b></div>
+			</div>
+            <a href="">Quên mật khẩu?</a>
+        </form>
+    </div>
+    <div class="footer">
+        <small>copyright @ NTTL-ĐTL</small>
+    </div>
+
+    
+</body>
+</html>
