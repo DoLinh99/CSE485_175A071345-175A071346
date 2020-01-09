ceea0cbd274d1de8ebfb5bd33eb0bfbc242bced3
175A071345 <lyntt72@wru.vn>
2020-01-09 15:47:16 +0700

    chinhsuabtl

---
+++ btl_cs/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/index.js"></script>
+
+    <title>ĐẠI HỌC THỦY LỢI</title>
+</head>
+<body>
+        <?php
+        include 'header.php';
+        ?>
+<div class="main">
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/dangbo1.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/nhanco.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/tet2020.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <div class="carousel-item">
+      <img src="img/truongdaihocthuyloi.jpg" class="d-block w-100" alt="...">
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+ 
+
+    <div id="cont">
+      <div class="row">
+     <div class="cont-left">
+       <h3>Tin tức</h3>
+       <div class="cont-left1">
+       <div class="tinchinh" class="primary">
+         <a href=""><img style="width:475px; height:275px" src="img/sach.jpg" alt=""></a>
+         <a href=""><h1>Bế giảng khóa học bồi dưỡng tiếng Anh cho công chức thuộc Bộ Nông nghiệp và Phát triển nông thôn năm 2019</h1></a>
+         <p>Vừa qua Trường Đại học Thủy lợi đã tổ chức bế giảng khóa học bồi dưỡng tiếng Anh cho công chức thuộc Bộ Nông nghiệp và Phát triển nông thôn năm 2019</p>
+       </div>
+       </div>
+       <div class="cont-left2">
+                <ul>
+                <li><a href="">Khai mạc đợt khảo sát chính thức phục vụ đánh giá ngoài 4 chương trình đào tạo</a></li>
+                <li><a href="">Đại học Thủy lợi là một trong 30 trường đại học Việt Nam có công bố quốc tế nhiều nhất</a></li>
+                <li><a href="">Bế giảng khóa học bồi dưỡng tiếng Anh cho công chức thuộc Bộ Nông nghiệp và Phát triển nông thôn năm 2019</a></li>
+                <li><a href="">Khai mạc giải bóng đá cán bộ, giảng viên thành phố Hà Nội năm 2019</a></li>
+                <li><a href="">Gặp mặt cán bộ viên chức nghỉ hưu năm 2019</a></li>
+                
+                </ul>
+            <a class="them" href="">Xem tất cả>></a>
+          </ul>
+       </div>
+     </div></div>
+     <div class="row1">
+     <div class="cont-right">
+       <h3>Thông báo</h3>
+       <div>
+         <ul>
+         <li><a href="">Thông báo về kế hoạch học kỳ 2 năm học 2019-2020...</a></li>
+         <li><a href="">Lịch thi tuyến sinh đào tạo trình độ thạc sĩ năm...</a></li>
+         <li><a href="">Thông báo các môn thi tuyển sinh trong đào tạo...</a></li>
+         <li><a href="">Lịch học chuyển đổi thi tuyển sinh cao học năm...</a></li>
+         <li><a href="">Thông báo tuyển sinh liên thông trình độ đại học...</a></li>
+           <a class="them1" href="">Xem thêm>></a>
+
+          </ul>
+       </div>
+     </div>
+   </div> </div></div>
+  <footer>
+        <?php
+        include 'footer.php'
+        ?>
+</footer>
+</body>
+</html>
